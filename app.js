3e0b680fb8304c4eb5b85dba4271d213af828f51
Alina Bregvadze <82210320+alinbrg@users.noreply.github.com>
2022-03-31 21:51:38 +0400

    lesson 13
     JavaScript variables, types

---
+++ app.js
@@ -0,0 +1,52 @@
+var userName = "nino";
+
+let itemCount = 0;
+const userName3 = "tea";
+const PI = 3.14;
+userName4 = "alina";
+// userName3 = 0;
+itemCount = 1;
+
+let userName2 = "giorgi";
+let userAge = 20;
+let myName;
+let isStudent = true;
+let myNull = null;
+let myNan = NaN;
+let mynan = NaN;
+// int userAge = 20;
+// NaN Not a Number;
+// console.log(typeof userAge, typeof isStudent, typeof userName, typeof myName);
+// console.log(userAg);
+/*
+console.log("myNull", typeof myNull);
+console.log("userAge", typeof userAge);
+console.log("isStudent", typeof isStudent);
+console.log("userName", typeof userName);
+console.log("myName", typeof myName);
+console.log("myNan", typeof myNan);
+*/
+
+let sum = 10 + 5;
+let multip = 10 * 5;
+let division = 10 / 5;
+let substraction = 10 - 5;
+
+console.log(sum, multip, division, substraction);
+
+let modulo = 10 % 4;
+// console.log(modulo);
+
+let power = 10 ** 2;
+// console.log(power);
+
+let myText = "examople text";
+let myNum = 5;
+
+let myNum1 = 10;
+let myNum2 = "10";
+
+console.log(myNum2 + myNum1);
+console.log(userName + " " + "გამარჯობა");
+let myTextEcample = `${userName} გამარჯობა`;
+console.log(myTextEcample);
